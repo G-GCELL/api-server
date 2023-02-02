73d4d59af9a4e7f1cb975d401a680dbf4b0b534f
김우용 <wykim@gabia.com>
2023-02-02 14:23:07 +0900

    Initial commit : Spring Boot Application

---
+++ src/main/java/com/gabia/weat/gcellapiserver/GcellApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.gabia.weat.gcellapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcellApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcellApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabia/weat/gcellapiserver/GcellApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabia.weat.gcellapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcellApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
