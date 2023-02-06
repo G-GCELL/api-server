2fd2b4f38683ec7cbefb285a14936e0348af1eed
tobi <tygwak@gabia.com>
2023-02-06 16:43:25 +0900

    Feat : POST /excels 엑셀 생성 요청 API 생성
     ExcelInfoController 생성 및 POST /excels 엑셀 생성 요청 API 구현

---
+++ src/main/java/com/gabia/weat/gcellapiserver/controller/ExcelInfoController.java
@@ -0,0 +1,35 @@
+package com.gabia.weat.gcellapiserver.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabia.weat.gcellapiserver.dto.APIResponseDTO;
+import com.gabia.weat.gcellapiserver.service.ExcelInfoService;
+
+import static com.gabia.weat.gcellapiserver.dto.FileDTO.FileCreateRequestDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/excels")
+public class ExcelInfoController {
+
+	private final ExcelInfoService excelInfoService;
+
+	@PostMapping(value = "")
+	@ResponseStatus(HttpStatus.CREATED)
+	public APIResponseDTO createExcel(@RequestBody FileCreateRequestDTO fileCreateRequestDTO){
+		Long createExcelInfoId = excelInfoService.createExcel(this.getConnectMemberEmail(), fileCreateRequestDTO);
+		return APIResponseDTO.success("/excels/" + createExcelInfoId);
+	}
+
+	private String getConnectMemberEmail(){
+		return "mock_email";
+	}
+
+}
---
+++ src/test/java/com/gabia/weat/gcellapiserver/controller/ExcelInfoControllerTest.java
@@ -0,0 +1,57 @@
+package com.gabia.weat.gcellapiserver.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.gabia.weat.gcellapiserver.dto.FileDTO.FileCreateRequestDTO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
+
+import com.gabia.weat.gcellapiserver.dto.APIResponseDTO;
+import com.gabia.weat.gcellapiserver.dto.FileDTO;
+import com.gabia.weat.gcellapiserver.service.ExcelInfoService;
+
+@ExtendWith(MockitoExtension.class)
+public class ExcelInfoControllerTest {
+
+	@Mock
+	private ExcelInfoService excelInfoService;
+	@InjectMocks
+	private ExcelInfoController excelInfoController;
+
+	@Test
+	@DisplayName("엑셀_생성_API_테스트")
+	public void createExcel_test(){
+		// given
+		Long testExcelInfoId = 1L;
+		FileCreateRequestDTO fileCreateRequestDTO = this.getFileCreateRequestDTO();
+
+		given(excelInfoService.createExcel(any(), any())).willReturn(testExcelInfoId);
+
+		// when
+		APIResponseDTO response = excelInfoController.createExcel(fileCreateRequestDTO);
+
+		// then
+		assertThat(response.getResponse()).isEqualTo("/excels/" + testExcelInfoId);
+	}
+
+	private FileCreateRequestDTO getFileCreateRequestDTO() {
+		return new FileDTO.FileCreateRequestDTO(
+			"testName",
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null);
+	}
+
+}
