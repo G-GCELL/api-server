5460ac6285186c8f34b0a6f2d48ac0a2ca9cfa44
tobi <tygwak@gabia.com>
2023-02-06 11:28:42 +0900

    Feat : ExcelInfoRepository Interface 생성
    ExcelInfoRepository Interface 생성

---
+++ src/main/java/com/gabia/weat/gcellapiserver/repository/ExcelInfoRepository.java
@@ -0,0 +1,10 @@
+package com.gabia.weat.gcellapiserver.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabia.weat.gcellapiserver.domain.ExcelInfo;
+
+public interface ExcelInfoRepository extends JpaRepository<ExcelInfo, Long> {
+	
+
+}
