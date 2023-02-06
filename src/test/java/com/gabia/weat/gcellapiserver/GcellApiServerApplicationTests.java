8ec228eea5b7fc2b48426ca92fa01ed54e4edd83
Aiden <yigwak@gabia.com>
2023-02-06 18:10:03 +0900

    Feat :  main

--- src/test/java/com/gabia/weat/gcellapiserver/GcellApiServerApplicationTests.java
+++ src/test/java/com/gabia/weat/gcellapiserver/GcellApiServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.gabia.weat.gcellapiserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestConstructor;
 
-@SpringBootTest
+@ActiveProfiles("test")
 class GcellApiServerApplicationTests {
 
     @Test
