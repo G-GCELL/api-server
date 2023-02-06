46877c702da88c29613c07c7473b471ac98451e5
tobi <tygwak@gabia.com>
2023-02-06 11:47:11 +0900

    Feat : FileDTO 생성
    FileDTO에 FileCreateRequestDTO record 생성

---
+++ src/main/java/com/gabia/weat/gcellapiserver/dto/FileDTO.java
@@ -0,0 +1,24 @@
+package com.gabia.weat.gcellapiserver.dto;
+
+import java.time.LocalDate;
+
+public class FileDTO {
+
+	public record FileCreateRequestDTO(
+		String fileName,
+		String[] inAccountId,
+		String[] notAccountId,
+		String[] inProductCode,
+		String[] notProductCode,
+		LocalDate startDateMin,
+		LocalDate startDateMax,
+		LocalDate endDateMin,
+		LocalDate endDateMax,
+		String costMin,
+		String costMax
+	){
+
+
+	}
+
+}
