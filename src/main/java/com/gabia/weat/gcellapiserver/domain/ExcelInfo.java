1f7e61c3b3c9c472147464daf270d1d09ee849ca
tobi <tygwak@gabia.com>
2023-02-06 10:15:19 +0900

    Feat : ExcelInfo Entity 생성
    ExcelInfo Entity Class 생성

---
+++ src/main/java/com/gabia/weat/gcellapiserver/domain/ExcelInfo.java
@@ -0,0 +1,42 @@
+package com.gabia.weat.gcellapiserver.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "excel_info")
+@Table(
+	uniqueConstraints = {
+		@UniqueConstraint(name = "member_name_unique", columnNames = {"member_id", "name"})
+	}
+)
+public class ExcelInfo extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long excelInfoId;
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	private Member member;
+	@Column
+	private String name;
+	@Column
+	private String path;
+	@Column
+	private Boolean isDeleted;
+
+}
