a623974ce0bbb3536e223499aae2ec33aeac75d7
tobi <tygwak@gabia.com>
2023-02-07 10:24:35 +0900

    Feat : SseRepositoryImpl save method 구현
    SseRepositoryImpl save method 구현

--- src/main/java/com/gabia/weat/gcellapiserver/repository/SseRepositoryImpl.java
+++ src/main/java/com/gabia/weat/gcellapiserver/repository/SseRepositoryImpl.java
@@ -6,22 +6,31 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Repository
-public class SseRepositoryImpl implements SseRepository{
+public class SseRepositoryImpl implements SseRepository {
 
-	private ConcurrentHashMap<Long, SseEmitter> sseMap;
-
-	public SseRepositoryImpl() {
-		this.sseMap = new ConcurrentHashMap<>();
-	}
+	private ConcurrentHashMap<Long, SseEmitter> sseMap = new ConcurrentHashMap<>();
 
 	@Override
 	public SseEmitter save(Long id, SseEmitter sseEmitter) {
-		return null;
+		sseMap.put(id, sseEmitter);
+		sseEmitter.onCompletion(() -> {
+			log.info(id + "`s sseEmitter onCompletion");
+			sseMap.remove(id);
+		});
+		sseEmitter.onTimeout(() -> {
+			log.info(id + "`s sseEmitter onTimeout");
+			sseEmitter.complete();
+		});
+		return sseEmitter;
 	}
 
 	@Override
 	public Optional<SseEmitter> findById(Long id) {
 		return Optional.empty();
 	}
+
 }
