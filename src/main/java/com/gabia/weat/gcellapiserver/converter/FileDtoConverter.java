abefdcdfe287748282220f53b3c7e3462cd1c719
tobi <tygwak@gabia.com>
2023-02-08 13:10:00 +0900

    Feat : FileDtoConverter Class 생성
    FileDtoConverter class 생성 및 createDtoToCreateMsgDto method 구현

--- src/main/java/com/gabia/weat/gcellapiserver/converter/FileDtoConverter.java
+++ src/main/java/com/gabia/weat/gcellapiserver/converter/FileDtoConverter.java
@@ -7,20 +7,23 @@ import com.gabia.weat.gcellapiserver.dto.MessageDto.FileCreateRequestMsgDto;
 
 public class FileDtoConverter {
 
-	public static FileCreateRequestMsgDto createDtoToCreateMsgDto(Long memberId, FileCreateRequestDto fileCreateRequestDTO){
+	public static FileCreateRequestMsgDto createDtoToCreateMsgDto(Long memberId, FileCreateRequestDto fileCreateRequestDto){
+		BigDecimal costMin = fileCreateRequestDto.costMin() == null? null : new BigDecimal(fileCreateRequestDto.costMin());
+		BigDecimal costMax = fileCreateRequestDto.costMax() == null? null : new BigDecimal(fileCreateRequestDto.costMax());
+		
 		return FileCreateRequestMsgDto.builder()
 			.memberId(memberId)
-			.fileName(fileCreateRequestDTO.fileName())
-			.inAccountId(fileCreateRequestDTO.inAccountId())
-			.notAccountId(fileCreateRequestDTO.notAccountId())
-			.inProductCode(fileCreateRequestDTO.inProductCode())
-			.notProductCode(fileCreateRequestDTO.notProductCode())
-			.startDateMin(fileCreateRequestDTO.startDateMin())
-			.startDateMax(fileCreateRequestDTO.startDateMax())
-			.endDateMin(fileCreateRequestDTO.endDateMin())
-			.endDateMax(fileCreateRequestDTO.endDateMax())
-			.costMin(new BigDecimal(fileCreateRequestDTO.costMin()))
-			.costMax(new BigDecimal(fileCreateRequestDTO.costMax()))
+			.fileName(fileCreateRequestDto.fileName())
+			.inAccountId(fileCreateRequestDto.inAccountId())
+			.notAccountId(fileCreateRequestDto.notAccountId())
+			.inProductCode(fileCreateRequestDto.inProductCode())
+			.notProductCode(fileCreateRequestDto.notProductCode())
+			.startDateMin(fileCreateRequestDto.startDateMin())
+			.startDateMax(fileCreateRequestDto.startDateMax())
+			.endDateMin(fileCreateRequestDto.endDateMin())
+			.endDateMax(fileCreateRequestDto.endDateMax())
+			.costMin(costMin)
+			.costMax(costMax)
 			.build();
 	}
 
