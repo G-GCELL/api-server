98aa18a8e7b0b41b973e38a8ce97bd315eaa18af
tobi <tygwak@gabia.com>
2023-02-08 10:41:31 +0900

    Style : MessageController Class 첫 줄 공백 처리
    MessageController Class 첫 줄 공백 처리

--- src/main/java/com/gabia/weat/gcellapiserver/controller/MessageController.java
+++ src/main/java/com/gabia/weat/gcellapiserver/controller/MessageController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
+
 	private final MessageService messageService;
 
 	@CrossOrigin("*")
