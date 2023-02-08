ab69f59cc529494ede5c07cf355812864476a2cd
tobi <tygwak@gabia.com>
2023-02-08 16:18:02 +0900

    Fix : GlobalExceptionHandler Class Error 처리 방식 변경
    Error 처리 방식에 오류가 있어서 현재 방식으로 변경

--- src/main/java/com/gabia/weat/gcellapiserver/error/GlobalExceptionHandler.java
+++ src/main/java/com/gabia/weat/gcellapiserver/error/GlobalExceptionHandler.java
@@ -1,17 +1,18 @@
 package com.gabia.weat.gcellapiserver.error;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.gabia.weat.gcellapiserver.dto.APIResponseDto;
-
-import io.minio.errors.MinioException;
+import com.gabia.weat.gcellapiserver.error.exception.CustomException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(MinioException.class)
-	private APIResponseDto minioException(ErrorCode errorCode) {
-		return APIResponseDto.fail(errorCode);
+	@ExceptionHandler(CustomException.class)
+	private ResponseEntity<APIResponseDto> customExceptionHandler(CustomException e) {
+		return ResponseEntity.status(e.getErrorCode().getStatus()).body(APIResponseDto.fail(e.getErrorCode()));
 	}
+
 }
