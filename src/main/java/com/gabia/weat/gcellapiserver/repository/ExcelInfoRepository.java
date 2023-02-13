31a56d842b93e02dc837e17e7f3d40de8785013d
tobi <tygwak@gabia.com>
2023-02-14 08:42:05 +0900

    Fix : ExcelInfoRepository findByIdAndMemberEmail method @Param 추가
    어노테이션이 없어 발생하는 문제 해결을 위해 추가

--- src/main/java/com/gabia/weat/gcellapiserver/repository/ExcelInfoRepository.java
+++ src/main/java/com/gabia/weat/gcellapiserver/repository/ExcelInfoRepository.java
@@ -8,12 +8,13 @@ import com.gabia.weat.gcellapiserver.domain.ExcelInfo;
 import com.gabia.weat.gcellapiserver.domain.Member;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ExcelInfoRepository extends JpaRepository<ExcelInfo, Long> {
 
 	Optional<ExcelInfo> findByMemberAndNameAndIsDeletedFalse(Member member, String name);
 
 	@Query("select e from excel_info e join fetch e.member m where e.excelInfoId =:excelInfoId and e.isDeleted = false and m.email =:email")
-	Optional<ExcelInfo> findByIdAndMemberEmail(Long excelInfoId, String email);
+	Optional<ExcelInfo> findByIdAndMemberEmail(@Param("excelInfoId") Long excelInfoId, @Param("email") String email);
 
 }
