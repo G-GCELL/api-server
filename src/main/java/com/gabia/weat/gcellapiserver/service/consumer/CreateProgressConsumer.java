8dc7d943176c47584184220b2c6f0a482474f3e9
tobi <tygwak@gabia.com>
2023-02-14 11:38:36 +0900

    Feat : CreateProgressConsumer SSE 진행률 메시지 전송 기능 구현
    클라이언트로 진행률을 전송하는 기능 추가

--- src/main/java/com/gabia/weat/gcellapiserver/service/consumer/CreateProgressConsumer.java
+++ src/main/java/com/gabia/weat/gcellapiserver/service/consumer/CreateProgressConsumer.java
@@ -7,25 +7,34 @@ import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
+import com.gabia.weat.gcellapiserver.domain.type.MessageType;
 import com.gabia.weat.gcellapiserver.dto.MessageDto.CreateProgressMsgDto;
+import com.gabia.weat.gcellapiserver.service.MessageService;
 import com.rabbitmq.client.Channel;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CreateProgressConsumer {
+
+	private final MessageService messageService;
 
 	@RabbitListener(queues = "${spring.rabbitmq.template.default-receive-queue}")
 	public void receiveMessage(CreateProgressMsgDto createProgressMsgDto, Channel channel,
 		@Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
-
-		// 임시 코드
-		log.info(String.valueOf(createProgressMsgDto));
-
-		// SSE 메시지 전송
-
+		sendCreateProgressMessage(createProgressMsgDto);
 		channel.basicAck(tag, false);
+	}
+
+	private void sendCreateProgressMessage(CreateProgressMsgDto createProgressMsgDto) {
+		messageService.sendMessageToMemberId(
+			createProgressMsgDto.memberId(),
+			MessageType.FILE_CREATION_PROGRESS,
+			createProgressMsgDto
+		);
 	}
 
 }
