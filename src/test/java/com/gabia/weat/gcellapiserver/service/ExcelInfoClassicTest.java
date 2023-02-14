73038384084dce94baa807d11b61dbc96bd1ab14
Aiden <yigwak@gabia.com>
2023-02-14 17:14:18 +0900

    Feat : 파일 조회 기능 Repository 테스트

--- src/test/java/com/gabia/weat/gcellapiserver/service/ExcelInfoClassicTest.java
+++ src/test/java/com/gabia/weat/gcellapiserver/service/ExcelInfoClassicTest.java
@@ -2,8 +2,11 @@ package com.gabia.weat.gcellapiserver.service;
 
 import com.gabia.weat.gcellapiserver.domain.ExcelInfo;
 import com.gabia.weat.gcellapiserver.domain.Member;
+import com.gabia.weat.gcellapiserver.dto.FileDto;
 import com.gabia.weat.gcellapiserver.repository.ExcelInfoRepository;
 import com.gabia.weat.gcellapiserver.repository.MemberRepository;
+import com.gabia.weat.gcellapiserver.repository.enums.CreatedAtCondition;
+import com.gabia.weat.gcellapiserver.repository.enums.NameCondition;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,10 +17,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doNothing;
+import static com.gabia.weat.gcellapiserver.dto.FileDto.FileListRequestDto;
+
+import java.awt.print.Pageable;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 public class ExcelInfoClassicTest {
@@ -31,22 +41,14 @@ public class ExcelInfoClassicTest {
 	private MinioService minioService;
 
 	private Member member;
-	private ExcelInfo excelInfo;
 
 	@BeforeEach
 	public void setUp() {
+
 		member = Member.builder()
 			.memberId(1L)
 			.email("test@gabia.com")
 			.name("testName")
-			.build();
-
-		excelInfo = ExcelInfo.builder()
-			.excelInfoId(1L)
-			.name("testName")
-			.path("testUrl")
-			.isDeleted(false)
-			.member(member)
 			.build();
 	}
 
@@ -54,15 +56,25 @@ public class ExcelInfoClassicTest {
 	@DisplayName("엑셀 파일 삭제 테스트")
 	public void deleteExcelFileTest() {
 		// given
+		ExcelInfo excelInfo = ExcelInfo.builder()
+			.excelInfoId(1L)
+			.name("testName")
+			.path("testUrl")
+			.isDeleted(false)
+			.member(member)
+			.build();
+
 		memberRepository.save(member);
 		excelInfoRepository.save(excelInfo);
+
 		doNothing().when(minioService).deleteExcel(excelInfo);
 
 		// when
-		excelInfo = excelInfoService.deleteExcelInfo(excelInfo.getMember().getEmail(), excelInfo.getExcelInfoId());
+		ExcelInfo deletedExcelInfo = excelInfoService.deleteExcelInfo(excelInfo.getMember().getEmail(), excelInfo.getExcelInfoId());
 
 		// then
-		assertThat(excelInfo.getIsDeleted()).isEqualTo(true);
+		assertThat(deletedExcelInfo.getIsDeleted()).isEqualTo(true);
 	}
+
 
 }
