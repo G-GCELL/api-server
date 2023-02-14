716dc911307435037cee3afd80a759895f4e3877
Aiden <yigwak@gabia.com>
2023-02-14 10:46:33 +0900

    Feat : Repository 메소드 이름 변경

--- src/main/java/com/gabia/weat/gcellapiserver/repository/ExcelInfoRepositoryCustom.java
+++ src/main/java/com/gabia/weat/gcellapiserver/repository/ExcelInfoRepositoryCustom.java
@@ -1,7 +1,5 @@
 package com.gabia.weat.gcellapiserver.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,6 +9,6 @@ import com.gabia.weat.gcellapiserver.dto.FileDto;
 
 public interface ExcelInfoRepositoryCustom {
 
-	Page<ExcelInfo> findByMember(Member member, FileDto.FileListRequestDto fileListRequestDto
+	Page<ExcelInfo> findByMemberPaging(Member member, FileDto.FileListRequestDto fileListRequestDto
 		, Pageable pageable);
 }
