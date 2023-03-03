e82b02eaef7b6664e9ef49b44e948a999dc4fa26
tobi <tygwak@gabia.com>
2023-03-03 13:31:12 +0900

    Feat : MessageHandler error handling 추가
    MessageHandler error handling 추가

--- src/main/java/com/gabia/weat/gcellapiserver/service/MessageHandler.java
+++ src/main/java/com/gabia/weat/gcellapiserver/service/MessageHandler.java
@@ -3,15 +3,16 @@ package com.gabia.weat.gcellapiserver.service;
 import static com.gabia.weat.gcellapiserver.dto.MessageDto.*;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gabia.weat.gcellapiserver.converter.MessageDtoConverter;
 import com.gabia.weat.gcellapiserver.domain.ExcelInfo;
+import com.gabia.weat.gcellapiserver.domain.type.MessageType;
 import com.gabia.weat.gcellapiserver.dto.MessageWrapperDto;
 import com.gabia.weat.gcellapiserver.error.ErrorCode;
 import com.gabia.weat.gcellapiserver.error.exception.CustomException;
 import com.gabia.weat.gcellapiserver.repository.ExcelInfoRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -24,18 +25,31 @@ public class MessageHandler {
 	@Transactional
 	public void sendCreateExcelMsg(MessageWrapperDto<FileCreateProgressMsgDto> messageWrapperDto) {
 		FileCreateProgressMsgDto message = messageWrapperDto.getMessage();
-		switch(message.messageType()){
-			case FILE_CREATION_PROGRESS -> messageSender.sendMessageToMemberId(message.memberId(), message.messageType(), message);
+		switch (message.messageType()) {
+			case FILE_CREATION_PROGRESS ->
+				messageSender.sendMessageToMemberId(message.memberId(), message.messageType(), message);
 			case FILE_CREATION_COMPLETE -> this.sendCreateCompleteMsg(message);
 		}
 	}
 
-	private void sendCreateCompleteMsg(FileCreateProgressMsgDto message){
+	@Transactional
+	public void sendErrorMsg(MessageWrapperDto<FileCreateRequestErrorMsgDto> messageWrapperDto) {
+		FileCreateRequestErrorMsgDto fileCreateRequestErrorMsgDto = messageWrapperDto.getMessage();
+		ExcelInfo excelInfo = excelInfoRepository.findById(fileCreateRequestErrorMsgDto.excelInfoId())
+			.orElseThrow(() -> new CustomException(ErrorCode.EXCEL_NOT_EXISTS));
+		excelInfoRepository.delete(excelInfo);
+		messageSender.sendMessageToExcelInfoId(fileCreateRequestErrorMsgDto.excelInfoId(), MessageType.ERROR,
+			fileCreateRequestErrorMsgDto);
+	}
+
+	private void sendCreateCompleteMsg(FileCreateProgressMsgDto message) {
 		ExcelInfo excelInfo = excelInfoRepository.findById(message.excelInfoId()).orElseThrow(() -> {
 			throw new CustomException(ErrorCode.EXCEL_NOT_EXISTS);
 		});
 		excelInfo.created();
-		FileCreateCompleteMsgDto fileCreateCompleteMsgDto = MessageDtoConverter.createProgressMsgToCreateCompleteMsg(message, excelInfo.getName());
+		excelInfoRepository.flush();
+		FileCreateCompleteMsgDto fileCreateCompleteMsgDto = MessageDtoConverter.createProgressMsgToCreateCompleteMsg(
+			message, excelInfo.getName());
 		messageSender.sendMessageToExcelInfoId(message.excelInfoId(), message.messageType(), fileCreateCompleteMsgDto);
 	}
 
--- src/test/java/com/gabia/weat/gcellapiserver/service/MessageHandlerTest.java
+++ src/test/java/com/gabia/weat/gcellapiserver/service/MessageHandlerTest.java
@@ -4,6 +4,7 @@ import static org.mockito.BDDMockito.*;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +14,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.gabia.weat.gcellapiserver.domain.ExcelInfo;
 import com.gabia.weat.gcellapiserver.domain.type.MessageType;
+import com.gabia.weat.gcellapiserver.dto.MessageDto;
 import com.gabia.weat.gcellapiserver.dto.MessageDto.FileCreateProgressMsgDto;
+import com.gabia.weat.gcellapiserver.dto.MessageDto.FileCreateRequestErrorMsgDto;
 import com.gabia.weat.gcellapiserver.dto.MessageWrapperDto;
 import com.gabia.weat.gcellapiserver.repository.ExcelInfoRepository;
 
@@ -26,6 +29,16 @@ public class MessageHandlerTest {
 	private ExcelInfoRepository excelInfoRepository;
 	@InjectMocks
 	private MessageHandler messageHandler;
+
+	private ExcelInfo excelInfo;
+
+	@BeforeEach
+	public void setUp(){
+		excelInfo = ExcelInfo.builder()
+			.excelInfoId(1L)
+			.name("test")
+			.build();
+	}
 
 	@Test
 	@DisplayName("엑셀_생성_진행률_메시지_전송_테스트")
@@ -47,16 +60,36 @@ public class MessageHandlerTest {
 		// given
 		FileCreateProgressMsgDto fileCreateProgressMsgDto = this.getFileCreateProgressMsgDto(MessageType.FILE_CREATION_COMPLETE);
 		MessageWrapperDto messageWrapperDto = MessageWrapperDto.wrapMessageDto(fileCreateProgressMsgDto, "");
-		ExcelInfo excelInfo = ExcelInfo.builder()
-			.excelInfoId(1L)
-			.name("test")
-			.build();
+
 		given(excelInfoRepository.findById(any())).willReturn(Optional.ofNullable(excelInfo));
 
 		// when
 		messageHandler.sendCreateExcelMsg(messageWrapperDto);
 
 		// then
+		verify(messageSender, times(1)).sendMessageToExcelInfoId(any(), any(), any());
+	}
+
+	@Test
+	@DisplayName("에러_메시지_전송_테스트")
+	public void sendErrorMsg_test(){
+		// given
+		FileCreateRequestErrorMsgDto fileCreateRequestErrorMsgDto = new FileCreateRequestErrorMsgDto(
+			1L,
+			excelInfo.getExcelInfoId(),
+			404,
+			"test_message"
+		);
+
+		MessageWrapperDto messageWrapperDto = MessageWrapperDto.wrapMessageDto(fileCreateRequestErrorMsgDto, "");
+
+		given(excelInfoRepository.findById(any())).willReturn(Optional.of(excelInfo));
+
+		// when
+		messageHandler.sendErrorMsg(messageWrapperDto);
+
+		// then
+		verify(excelInfoRepository, times(1)).delete(any());
 		verify(messageSender, times(1)).sendMessageToExcelInfoId(any(), any(), any());
 	}
 
