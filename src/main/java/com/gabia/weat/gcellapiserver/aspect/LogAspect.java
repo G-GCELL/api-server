098f8efaabfa5bd3ff75c0ede4911d2888bf4b83
tobi <tygwak@gabia.com>
2023-03-07 16:27:42 +0900

    Refactor : Log 출력 로직 LogPrinter로 이동
    공통적으로 사용되는 Log 출력 로직을 LogPrinter로 이동

--- src/main/java/com/gabia/weat/gcellapiserver/aspect/LogAspect.java
+++ src/main/java/com/gabia/weat/gcellapiserver/aspect/LogAspect.java
@@ -4,10 +4,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.CodeSignature;
-import org.slf4j.event.Level;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -19,13 +15,10 @@ import com.gabia.weat.gcellapiserver.annotation.ConsumerLog;
 import com.gabia.weat.gcellapiserver.annotation.ProducerLog;
 import com.gabia.weat.gcellapiserver.domain.type.TargetType;
 import com.gabia.weat.gcellapiserver.dto.MessageWrapperDto;
-import com.gabia.weat.gcellapiserver.dto.log.ApiLogFormatDto.ApiLogFormatDtoBuilder;
 import com.gabia.weat.gcellapiserver.dto.log.LogFormatFactory;
-import com.gabia.weat.gcellapiserver.dto.log.MessageBrokerLogFormatDto.MessageBrokerLogFormatDtoBuilder;
 import com.gabia.weat.gcellapiserver.error.ErrorCode;
 import com.gabia.weat.gcellapiserver.error.exception.CustomException;
 import com.gabia.weat.gcellapiserver.service.log.LogPrinter;
-import com.gabia.weat.gcellapiserver.parser.CustomExpressionParser;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -35,46 +28,42 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LogAspect {
 
-	private final String X_FORWARDED_FOR_HEADER = "X-FORWARDED-FOR";
 	private final LogFormatFactory logFormatFactory;
 	private final LogPrinter logPrinter;
-	private final CustomExpressionParser expressionBeanParser;
 
 	@Around("within(com.gabia.weat.gcellapiserver.controller..*)")
 	public Object apiLogAdvisor(ProceedingJoinPoint joinPoint) throws Throwable {
 		logFormatFactory.startTrace();
-		ApiLogFormatDtoBuilder logFormatBuilder = this.getDefaultApiLogFormatBuilder();
 		long startTime = System.currentTimeMillis();
+		boolean success = true;
+		int status = 200;
+		int detailStatus = 0;
 		try {
 			Object result = joinPoint.proceed();
-			this.successStatus(logFormatBuilder, result);
 			return result;
 		} catch (CustomException e) {
-			this.failStatus(logFormatBuilder, e.getErrorCode());
+			status = e.getErrorCode().getStatus().value();
+			detailStatus = e.getErrorCode().getCustomStatus().getCode();
 			throw e;
 		} catch (Exception e) {
 			ErrorCode errorCode = ErrorCode.UNKNOWN_ERROR;
-			this.failStatus(logFormatBuilder, errorCode);
-			this.printErrorLog(e);
+			status = errorCode.getStatus().value();
+			detailStatus = errorCode.getCustomStatus().getCode();
+			logPrinter.printErrorLog(e);
 			throw new CustomException(errorCode);
 		} finally {
 			long time = System.currentTimeMillis() - startTime;
 			String input = this.getInput(joinPoint);
-			this.printApiLog(logFormatBuilder, time, input);
+			logPrinter.printApiLog(this.getRequestInfo(), success, status, detailStatus, time, input);
 			logFormatFactory.endTrace();
 		}
 	}
 
 	@Around("@annotation(consumerLog)")
 	public Object consumerLogAdvisor(ProceedingJoinPoint joinPoint, ConsumerLog consumerLog) throws Throwable {
-		try {
-			this.setTraceId(joinPoint);
-			this.printMessageBrokerLog(TargetType.CONSUMER, consumerLog.queue(), this.getInput(joinPoint), null);
-			return joinPoint.proceed();
-		} catch (Exception e) {
-			this.printErrorLog(e);
-			throw e;
-		}
+		this.setTraceId(joinPoint);
+		logPrinter.printMessageBrokerLog(TargetType.CONSUMER, consumerLog.queue(), this.getInput(joinPoint), null);
+		return joinPoint.proceed();
 	}
 
 	@Around("@annotation(producerLog)")
@@ -86,8 +75,12 @@ public class LogAspect {
 			exception = e;
 			throw e;
 		} finally {
-			this.printMessageBrokerLog(TargetType.PRODUCER, producerLog.exchange(), this.getInput(joinPoint),
-				exception);
+			logPrinter.printMessageBrokerLog(
+				TargetType.PRODUCER,
+				producerLog.exchange(),
+				this.getInput(joinPoint),
+				exception
+			);
 		}
 	}
 
@@ -99,35 +92,6 @@ public class LogAspect {
 				break;
 			}
 		}
-	}
-
-	private HttpServletRequest getRequestInfo() {
-		return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
-	}
-
-	private ApiLogFormatDtoBuilder getDefaultApiLogFormatBuilder() {
-		HttpServletRequest request = this.getRequestInfo();
-		return logFormatFactory.getApiLogFormatBuilder()
-			.httpMethod(HttpMethod.valueOf(request.getMethod()))
-			.userIp(this.getRemoteAddr(request))
-			.apiUri(request.getRequestURI());
-	}
-
-	protected String getRemoteAddr(HttpServletRequest request) {
-		String header = request.getHeader(X_FORWARDED_FOR_HEADER);
-		return (header != null) ? header : request.getRemoteAddr();
-	}
-
-	private void successStatus(ApiLogFormatDtoBuilder logFormatBuilder, Object result) {
-		int status = result instanceof ResponseEntity response ?
-			response.getStatusCode().value() : HttpStatus.OK.value();
-		logFormatBuilder.success(true).status(status);
-	}
-
-	private void failStatus(ApiLogFormatDtoBuilder logFormatBuilder, ErrorCode errorCode) {
-		logFormatBuilder.success(false)
-			.status(errorCode.getStatus().value())
-			.detailStatus(errorCode.getCustomStatus().getCode());
 	}
 
 	private String getInput(ProceedingJoinPoint joinPoint) {
@@ -158,35 +122,8 @@ public class LogAspect {
 		}
 	}
 
-	private void printApiLog(ApiLogFormatDtoBuilder logFormatBuilder, long time, String input) {
-		logFormatBuilder
-			.time(time)
-			.input(input)
-			.build();
-		logPrinter.print(logFormatBuilder.build());
-	}
-
-	private void printErrorLog(Exception e) {
-		StackTraceElement stackTraceElement = e.getStackTrace()[0];
-		logPrinter.print(logFormatFactory.getErrorLogFormatBuilder()
-			.className(stackTraceElement.getClassName())
-			.methodName(stackTraceElement.getMethodName())
-			.exceptionName(e.getClass().getName())
-			.message(e.getMessage())
-			.build());
-	}
-
-	private void printMessageBrokerLog(TargetType type, String target, String input, Exception exception) {
-		String targetName = (String)expressionBeanParser.parse(target);
-		MessageBrokerLogFormatDtoBuilder logFormatDtoBuilder = logFormatFactory.getMessageBrokerLogFormatBuilder()
-			.level(exception == null? Level.INFO : Level.ERROR)
-			.type(type)
-			.exchangeName(type == TargetType.PRODUCER ? targetName : null)
-			.queueName(type == TargetType.CONSUMER ? targetName : null)
-			.exception(exception)
-			.input(input);
-
-		logPrinter.print(logFormatDtoBuilder.build());
+	private HttpServletRequest getRequestInfo() {
+		return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
 	}
 
 }
