c418a6b33a1fa22ae021f6f54cf5f0b54d95615a
tobi <tygwak@gabia.com>
2023-03-14 14:28:32 +0900

    Feat : token cookie 저장 방식으로 변경
    token cookie 저장 방식으로 변경

--- src/main/java/com/gabia/weat/gcellapiserver/controller/AuthController.java
+++ src/main/java/com/gabia/weat/gcellapiserver/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.gabia.weat.gcellapiserver.controller;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gabia.weat.gcellapiserver.dto.ApiResponseDto;
 import com.gabia.weat.gcellapiserver.dto.OauthDto.OauthLoginResponseDto;
+import com.gabia.weat.gcellapiserver.jwt.JwtProperty;
 import com.gabia.weat.gcellapiserver.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +23,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+	@Value("${redirect-page-url}")
+	private String redirectPageUrl;
 	private final AuthService authService;
+	private final JwtProperty jwtProperty;
 
 	@GetMapping(value = "/oauth2/authorization/hiworks")
 	public ResponseEntity<ApiResponseDto> redirectHiworksAuth() {
@@ -30,9 +37,23 @@ public class AuthController {
 	}
 
 	@GetMapping(value = "/login/oauth2/code/hiworks")
-	public ResponseEntity<ApiResponseDto> oauthLogin(@RequestParam("auth_code") String authCode) {
+	public ResponseEntity<ApiResponseDto> oauthLogin(@RequestParam("auth_code") String authCode) throws
+		URISyntaxException {
 		OauthLoginResponseDto oauthLoginResponseDto = authService.oauthLogin(authCode);
-		return ResponseEntity.ok(ApiResponseDto.success(oauthLoginResponseDto));
+		return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(setTokenCookieHeader(oauthLoginResponseDto)).build();
+	}
+
+	private HttpHeaders setTokenCookieHeader(OauthLoginResponseDto oauthLoginResponseDto) throws URISyntaxException {
+		System.out.println(oauthLoginResponseDto.accessToken());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(new URI(redirectPageUrl));
+		ResponseCookie cookie = ResponseCookie.from("token", oauthLoginResponseDto.accessToken())
+			.maxAge(jwtProperty.getAccessTokenExpiration())
+			.path("/")
+			.httpOnly(true)
+			.build();
+		headers.set(HttpHeaders.SET_COOKIE, cookie.toString());
+		return headers;
 	}
 
 }
