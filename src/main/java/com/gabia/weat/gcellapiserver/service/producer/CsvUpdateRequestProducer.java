64543892681ced1cbbe621d622e7a66d915e8588
김우용 <wykim@gabia.com>
2023-03-14 14:54:09 +0900

    Refactor : sendMessage 예외 처리 수정 AmqpException을 CustomException으로 변환하도록 수정하였음. ErrorCode 및 CustomStatus 추가하였음.

--- src/main/java/com/gabia/weat/gcellapiserver/service/producer/CsvUpdateRequestProducer.java
+++ src/main/java/com/gabia/weat/gcellapiserver/service/producer/CsvUpdateRequestProducer.java
@@ -1,12 +1,14 @@
 package com.gabia.weat.gcellapiserver.service.producer;
 
-import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import com.gabia.weat.gcellapiserver.annotation.ProducerLog;
 import com.gabia.weat.gcellapiserver.dto.MessageDto.CsvUpdateRequestMsgDto;
 import com.gabia.weat.gcellapiserver.dto.MessageWrapperDto;
+import com.gabia.weat.gcellapiserver.error.ErrorCode;
+import com.gabia.weat.gcellapiserver.error.exception.CustomException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,8 +21,11 @@ public class CsvUpdateRequestProducer implements Producer<CsvUpdateRequestMsgDto
 	@Override
 	@ProducerLog(exchange = "${rabbitmq.exchange.direct-exchange}")
 	public void sendMessage(MessageWrapperDto<CsvUpdateRequestMsgDto> message) {
-		CorrelationData correlationData = new CorrelationData();
-		csvUpdateRequestRabbitTemplate.correlationConvertAndSend(message, correlationData);
+		try {
+			csvUpdateRequestRabbitTemplate.convertAndSend(message);
+		} catch (AmqpException e) {
+			throw new CustomException(ErrorCode.MESSAGE_BROKER_CONNECTION_ERROR);
+		}
 	}
 
 }
